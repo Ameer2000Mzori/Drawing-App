b60a7c06e3dd2866b8cc3e9146b025924b8b5966
Ameer <ameerapex496@gmail.com>
2023-12-03 17:58:29 +0100

    added typescript code to the project

--- src/script.js
+++ src/script.js
@@ -1,5 +1,11 @@
 // selecting our elements
 var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
 // our global varibales
+var size = 20;
 // our functions
+var drawingCircle = function (x, y) {
+    ctx.beginPath();
+    ctx.arc(x, y, size, 0, Math.PI * 2, true);
+};
 // our eventlisnters
--- src/script.ts
+++ src/script.ts
@@ -1,8 +1,14 @@
 // selecting our elements
-const canvas = document.getElementById("canvas") as HTMLElement;
-
+const canvas = document.getElementById("canvas") as any;
+const ctx = canvas.getContext("2d");
 // our global varibales
 
+let size = 20;
+
 // our functions
+const drawingCircle = (x, y) => {
+  ctx.beginPath();
+  ctx.arc(x, y, size, 0, Math.PI * 2, true);
+};
 
 // our eventlisnters
