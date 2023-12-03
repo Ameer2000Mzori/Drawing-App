4d4d0e41d711762875154ef5a200dc61e7f85644
Ameer <ameerapex496@gmail.com>
2023-12-03 18:06:20 +0100

    added h and w within html for canvans

--- src/script.js
+++ src/script.js
@@ -2,10 +2,14 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 // our global varibales
-var size = 20;
+var size = 5;
+var color = "black";
 // our functions
 var drawingCircle = function (x, y) {
     ctx.beginPath();
     ctx.arc(x, y, size, 0, Math.PI * 2, true);
+    ctx.fillStyle = color;
+    ctx.fill();
 };
+drawingCircle(100, 200);
 // our eventlisnters
--- src/script.ts
+++ src/script.ts
@@ -3,12 +3,16 @@ const canvas = document.getElementById("canvas") as any;
 const ctx = canvas.getContext("2d");
 // our global varibales
 
-let size = 20;
-
+let size = 5;
+let color = "black";
 // our functions
-const drawingCircle = (x, y) => {
+const drawingCircle = (x: any, y: any) => {
   ctx.beginPath();
   ctx.arc(x, y, size, 0, Math.PI * 2, true);
+  ctx.fillStyle = color;
+  ctx.fill();
 };
+
+drawingCircle(100, 200);
 
 // our eventlisnters
