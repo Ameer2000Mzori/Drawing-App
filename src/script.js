7c99ea1b731be518a50ee49102d2d26afcd4d023
Ameer <ameerapex496@gmail.com>
2023-12-03 18:10:37 +0100

    added drawLine function to the project

--- src/script.js
+++ src/script.js
@@ -4,12 +4,23 @@ var ctx = canvas.getContext("2d");
 // our global varibales
 var size = 5;
 var color = "black";
+var x;
+var y;
 // our functions
 var drawingCircle = function (x, y) {
     ctx.beginPath();
     ctx.arc(x, y, size, 0, Math.PI * 2, true);
     ctx.fillStyle = color;
     ctx.fill();
+};
+// draw line function
+var drawLine = function (x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = size;
+    ctx.stroke();
 };
 drawingCircle(100, 200);
 // our eventlisnters
--- src/script.ts
+++ src/script.ts
@@ -5,12 +5,25 @@ const ctx = canvas.getContext("2d");
 
 let size = 5;
 let color = "black";
+let x: any;
+let y: any;
 // our functions
 const drawingCircle = (x: any, y: any) => {
   ctx.beginPath();
   ctx.arc(x, y, size, 0, Math.PI * 2, true);
   ctx.fillStyle = color;
   ctx.fill();
+};
+
+// draw line function
+
+const drawLine = (x1: any, y1: any, x2: any, y2: any) => {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.strokeStyle = color;
+  ctx.lineWidth = size;
+  ctx.stroke();
 };
 
 drawingCircle(100, 200);
